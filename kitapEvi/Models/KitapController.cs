74084e924c9b9f1eb0d8079f311693cfa4018588
mehmet444 <mehmet444@BPC08>
2024-02-15 09:15:51 +0300

    Proje dosyası ekle.

---
+++ kitapEvi/Models/KitapController.cs
@@ -0,0 +1,9 @@
+﻿namespace kitapEvi.Models
+{
+    public class KitapController
+    {
+        public int KategoriId { get; set; }
+        public string KategoriAdi { get; set; }
+        List<KitapController> KitapControllers = new List<KitapController>();
+    }
+}
